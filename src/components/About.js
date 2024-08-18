fe5bf1eefd6d363548cf6dd56c5fd330183dbb6b
T-Chol <taban.chol@student.moringaschool.com>
2024-08-18 17:29:22 +0300

    react-hooks-jsx-lab

--- src/components/About.js
+++ src/components/About.js
@@ -1,8 +1,14 @@
 import React from "react";
 import { image } from "../data/data";
 
-function About() {
-  return <div>About</div>;
+function about() {
+  return(
+  <div id="about">
+    <h2>About Me</h2>
+      <p>Holy cow!</p>
+      <img src={image} alt="I made this" />
+  </div>
+  )
 }
 
-export default About;
+export default about;
